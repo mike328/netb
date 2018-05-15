a0122616a726434597a733caae5c1b311f0965aa
mike328 <michael.abebe2@gmail.com>
2018-05-15 10:22:14 +0300

    first test

---
+++ src/calculatormodel/Tester.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatormodel;
+
+/**
+ *
+ * @author Michael
+ */
+public class Tester {
+    public static void main(String[] args) {
+        CalculatoView view = new CalculatoView();
+        CalculatoModel model = new CalculatoModel();
+        new ClaculateConroler(view, model);
+        view.setVisible(true);
+    }
+}
